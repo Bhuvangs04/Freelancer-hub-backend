e19fc30b85dd51fb561fa25dad2b459cbad6f351
Bhuvangs04 <bhuvangs2004@gmail.com>
2025-02-27 12:16:56 +0530

    Updated Auth.js

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,3 +1,4 @@
+const { errorMonitor } = require("http-proxy");
 const JWT = require("jsonwebtoken");
 const Secret = "SecureOnlyPassword";
 
@@ -14,7 +15,16 @@ async function createTokenForUser(user) {
 async function verifyToken(req, res, next) {
   const token = req.cookies.token;
   if (!token) {
-    return res.status(403).send({ error: "Unauthorized." });
+    return res.status(403).send({
+      errorType: "No Direct Access Allowed",
+      message: "Please login to access this resource.",
+      errorCode: 403,
+      errorStatus: "Forbidden",
+      errorDescription: "You are not authorized to access this resource.",
+      errorMonitor: "PublicVisibleBanned",
+      errorSolution: "Please login to access this resource.",
+      errorReference: "http://localhost:8080/sign-in",
+    });
   }
   try {
     const decoded = JWT.verify(token, Secret);
@@ -31,7 +41,8 @@ async function verifyToken(req, res, next) {
 }
 
 const authorize = (roles) => (req, res, next) => {
-  if (!roles.includes(req.user.role)) return res.status(403).send({message:"Forbidden"});
+  if (!roles.includes(req.user.role))
+    return res.status(403).send({ message: "Forbidden" });
   next();
 };
 module.exports = { createTokenForUser, verifyToken, authorize };
