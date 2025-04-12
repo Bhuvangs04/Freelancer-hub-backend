21e56c916dfa1c54b47bd3884ec5d74698ed43f5
Bhuvan G Sangappanavar <bhuvangs2004@gmail.com>
2025-04-12 23:42:51 +0530

    Update freelancer.js

--- routes/freelancer.js
+++ routes/freelancer.js
@@ -307,10 +307,10 @@ router.post(
       const project = await Project.findById(projectId);
       if (!project)
         return res.status(404).json({ message: "Project not found" });
-      if (project.status != "completed")
-        return res
-          .status(400)
-          .json({ message: "Project is not completed yet" });
+      // if (project.status != "completed")
+      //   return res
+      //     .status(400)
+      //     .json({ message: "Project is not completed yet" });
       const review = new ReviewSchema({
         reviewedId: req.user.userId,
         reviewerId: project.clientId,
