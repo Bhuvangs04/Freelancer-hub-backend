f52cbf6c0c5d60988c4b58e1fe6c3e8c72c28d18
Bhuvan G Sangappanavar <bhuvangs2004@gmail.com>
2025-04-21 11:01:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -210,7 +210,7 @@ app.use("/api/vi/client", client);
 app.use("/api/vi", loginRoute);
 app.use("/api/vi", signupRoute);
 app.use("/api/vi", uploadRoute);
-app.use("/api/vi/admin", admin);
+app.use("/admin", admin);
 app.use("/api/vi/freelancer", freelancer);
 app.use("/api/vi/payments", payment);
 app.use("/api/vi/worksubmission", workSubmission);
